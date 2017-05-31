9db452afb7717a130b79ad39f16bf57791e1f3c6
jpeumesmo <joaopmoferreira@gmail.com>
2017-05-31 17:10:38 -0300

    começo back-end

---
+++ ChampionsLinux/src/br/edu/ufsj/ccomp/tecweb/services/Usuario.java
@@ -0,0 +1,16 @@
+package br.edu.ufsj.ccomp.tecweb.services;
+
+public class Usuario {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
